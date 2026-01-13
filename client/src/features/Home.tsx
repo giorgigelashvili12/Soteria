6d79a949dbe31732e754c8e49ddc5cb67aeb0391
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-01-13 20:13:48 +0400

    bug patched

--- client/src/features/Home.tsx
+++ client/src/features/Home.tsx
@@ -1,14 +1,7 @@
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { Toaster } from "sonner";
-import { useEffect } from "react";
 
 export default function Home() {
-  const nav = useNavigate();
-
-  useEffect(() => {
-    nav("/");
-  }, [nav]);
-
   return (
     <>
       <Toaster position="bottom-left" richColors />
