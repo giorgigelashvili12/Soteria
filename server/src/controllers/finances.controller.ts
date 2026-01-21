68ab547a4c7079daa126dab28f6101b99d0e2554
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-01-21 21:43:18 +0400

    Testing - Product Catalog & Balance

--- server/src/controllers/finances.controller.ts
+++ server/src/controllers/finances.controller.ts
@@ -6,10 +6,13 @@ import PaymentIntent from "../models/PaymentIntent.model.js";
 export const stats = async (req: Request, res: Response) => {
   //@ts-ignore
   const merchant = req.merchant._id || req.merchant.id;
+  console.log(merchant);
   const balance = await Balance.findOne({ account_id: merchant });
+  console.log(balance);
   const transactions = await Transaction.find({ account_id: merchant })
     .sort({ createdAt: -1 })
     .limit(10);
+  console.log(transactions);
 
   const intents = await PaymentIntent.find({
     //@ts-ignore
