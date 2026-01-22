82aac5c20043bd7d9798fa0f6aed1c9481b886fb
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-01-22 22:07:33 +0400

    Fixed Checkout - Display Cart Items

--- server/src/index.ts
+++ server/src/index.ts
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 // routes
 app.use("/api/v1", router);
 
-app.get("*", (req, res) => {
+app.get("(.*)", (req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
