ed51ebaeb8d00376e7733e138aee953f767741e8
giorgigelashvili12 <123294128+giorgigelashvili12@users.noreply.github.com>
2026-01-23 19:21:48 +0400

    Fixed Redirection Setup

--- server/src/routes/metrics.route.ts
+++ server/src/routes/metrics.route.ts
@@ -1,8 +1,9 @@
 import { grossVolume } from "../controllers/metrics.controller.js";
 import { Router } from "express";
+import { protect } from "../middleware/protect.middleware.js";
 
 const router = Router();
 
-router.get("/gross-volume", grossVolume);
+router.get("/gross-volume", protect, grossVolume);
 
 export default router;
